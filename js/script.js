8fa5ecc35aecec0d1ad5f4cbcad275131a39f408
Artem Parkhomenko <parhom1011@yandex.ru>
2022-12-07 16:51:13 +0300

    Used conditions and cicles 07122022

--- js/script.js
+++ js/script.js
@@ -10,12 +10,57 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+/* let i = 0;
+while (i < 2) {
+    i++;
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+        
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+} */
+
+/* let i = 0;
+do {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+  
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+    } else {
+    console.log('error');
+    i--;
+    }
+    i++;
+} while (i < 2); */
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов!');
+} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+    console.log('Ты классический зритель!');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Ты наркоман!');
+} else {
+    console.log('Упс, ошибка!');
+}
 
 console.log(personalMovieDB);
